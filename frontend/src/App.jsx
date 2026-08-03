25224bfed1856b44a4511a8c1163252e50612698
Balam Chauhan <coding34018@gmail.com>
2026-08-04 03:09:29 +0530

    fix dashboard folder case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/authentication/login/Login";
 import Signup from "./components/authentication/signup/Signup";
-import Dashboard from "./components/Dashboard/Dashboard";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+
 import Loader from "./utils/Loader";
 import ProtectedRoute from "./components/Dashboard/Protected";
 import { userContext } from "./context/UserContext";
